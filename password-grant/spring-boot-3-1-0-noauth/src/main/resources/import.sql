19b14026b7990059e26defe2ffd05c50a4949e1e
Eduardo Maia <eduardobmaia@hotmail.com>
2025-01-05 10:58:57 -0300

    BCrypt password

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$r0KHhXbktmEVdK05OKZ/9OsZ4iFRe5gx3Ktm43QXbFheZ9SuuZfQy');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$r0KHhXbktmEVdK05OKZ/9OsZ4iFRe5gx3Ktm43QXbFheZ9SuuZfQy');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
